691e158744c726a3a541b54d525dfcbdc4306420
mohamedsayed18 <mohamed95.a.s@gmail.com>
2021-11-09 17:33:53 +0100

    working ext_axis

--- abb_example/src/abb_planner_example.cpp
+++ abb_example/src/abb_planner_example.cpp
@@ -143,23 +143,23 @@ namespace tesseract_ros_examples
     joint_names.push_back("joint_4");
     joint_names.push_back("joint_5");
     joint_names.push_back("joint_6");
-    // joint_names.push_back("positioner_base_joint");
-    // joint_names.push_back("positioner_joint_1");
+    joint_names.push_back("positioner_base_joint");
+    joint_names.push_back("positioner_joint_1");
 
-    Eigen::VectorXd joint_start_pos(6);
+    Eigen::VectorXd joint_start_pos(8);
     joint_start_pos(0) = -0.4;
     joint_start_pos(1) = 0.2762;
     joint_start_pos(2) = 0.0;
     joint_start_pos(3) = -1.3348;
     joint_start_pos(4) = 0.0;
     joint_start_pos(5) = 1.4959;
-    // joint_start_pos(6) = 0;
-    // joint_start_pos(7) = 0;
+    joint_start_pos(6) = 0;
+    joint_start_pos(7) = 0;
 
     env_->setState(joint_names, joint_start_pos);
 
     // Create manipulator information for program
-    ManipulatorInfo mi("robot_only", "base_link", "tool0");
+    ManipulatorInfo mi("manipulator", "positioner_tool0", "tool0");
 
     // Create Program
     CompositeInstruction program("FREESPACE", CompositeInstructionOrder::ORDERED, mi);
